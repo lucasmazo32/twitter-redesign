0dcbe50581f30ef2fc61ae67d3c2860635e4cb20
lucasmazo32 <lucasmazo32@gmail.com>
2020-02-21 11:16:48 -0500

    finished validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,25 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
+  validate :good_username
+
   has_many :opinions, foreign_key: 'author', class_name: 'Opinion'
   has_many :follows, foreign_key: 'follower', class_name: 'Opinion'
   has_many :followds, foreign_key: 'followed', class_name: 'Opinion'
 
   validates :username, presence: true, allow_blank: false,
                        uniqueness: { case_sensitive: false },
-                       length: { in: 5..20 }
+                       length: { in: 5..20 },
+                       format: { without: /\s/, message: 'no spaces allowed' }
   validates :name, presence: true, allow_blank: nil, length: { in: 4..25 }
+
+  private
+
+  def good_username
+    prof_filter = LanguageFilter::Filter.new(
+      matchlist: :profanity,
+      replacement: :stars
+    )
+    return unless prof_filter.match? username
+
+    errors.add(:username, "The following word(s) are forbidden: #{prof_filter.matched(username).join(', ')}")
+  end
 end
