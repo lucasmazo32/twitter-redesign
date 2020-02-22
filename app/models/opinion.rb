eb16ded4a22ad562da496b58373ad67d217dfa85
lucasmazo32 <lucasmazo32@gmail.com>
2020-02-22 13:46:53 -0500

    finished converting plain text into a hyperlink.

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -4,6 +4,19 @@ class Opinion < ApplicationRecord
   belongs_to :author, class_name: 'User'
   validates :text, presence: true, length: { in: 1..200 }
 
+  def link_jlt
+    scanned = text.scan(/jlt_\w+/)
+    return self.text if scanned.empty?
+    scanned = scanned.map{ |x| x[4...x.length] }
+    scanned.each do |username|
+      if user = User.find_by(username: username)
+        text = "<a href='/me/#{username}'>jlt_#{username}</a>"
+        self.text = self.text.gsub("jlt_#{username}", text)
+      end
+    end
+    self.text.gsub(URI.regexp, '<a href="\0">\0</a>').html_safe
+  end
+
   private
 
   def languagefilter
--- test.rb
+++
@@ -1,8 +0,0 @@
-def test(seed = false)
-  @user = 5 if seed
-  @user
-end
-
-p test(true)
-
-p test
